f09f1d809f1026e5dc1d39129f40fea2be4a229e
IDEAXA <59970605+IDEAXA@users.noreply.github.com>
2020-10-26 14:12:28 +0530

    MQTT CODE UPDATE
    mqtt code added

---
+++ IOT_VER_1.0_MQTT/iot_config.h
@@ -0,0 +1,22 @@
+//--------------------------IOT_VER_1.0---------------------------------------
+//--------------------------BASIC BOARD CONFIGURATION-------------------------
+#define RELAY1 25
+#define RELAY2 26
+#define RELAY3 27
+#define RELAY4 33
+#define DHT11 2
+#define BUTTON_PIN 33
+#define OLED_ADD 0x3C
+#define LCD_ADD 0x3C
+#define RGB_R 15
+#define RGB_G 2
+#define RGB_B 5
+#define debug true
+//--------------------------WIFI CONFIGURATION------------------------------
+#define SSID 65413241
+#define PSWD 66416541
+//--------------------------MQTT CONFIGURATION------------------------------
+#define MQTT_URL ihkijnhklj
+#define MQTT_USR 654654
+#define MQTT_PSWD 6546354
+#define MQTT_PORT 1883
