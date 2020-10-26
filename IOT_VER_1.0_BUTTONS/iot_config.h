9b8bfd10cb96dafb666f21cc8579db1a36020cfe
IDEAXA <59970605+IDEAXA@users.noreply.github.com>
2020-10-26 14:12:05 +0530

    Buttons code update
    we added button code checked with onboard LCD

---
+++ IOT_VER_1.0_BUTTONS/iot_config.h
@@ -0,0 +1,22 @@
+//--------------------------IOT_VER_1.0---------------------------------------
+//--------------------------BASIC BOARD CONFIGURATION-------------------------
+#define RELAY1 25
+#define RELAY2 26
+#define RELAY3 27
+#define RELAY4 34
+#define DHT11 2
+#define BUTTON_PIN 34
+#define OLED_ADD 0x3C
+#define LCD_ADD 0x3C
+#define RGB_R 15
+#define RGB_G 2
+#define RGB_B 5
+#define debug true
+//--------------------------WIFI CONFIGURATION------------------------------
+#define SSID ********
+#define PSWD ********
+//--------------------------MQTT CONFIGURATION------------------------------
+#define MQTT_URL ********
+#define MQTT_USR ********
+#define MQTT_PSWD ********
+#define MQTT_PORT *******
